5fc1dc7c3a7a89d721ee51cdaa6e005cb9323e72
Vladimir Pavlov <volodia.pavloff201343@gmail.com>
2022-02-19 13:44:03 +0300

    Add upload to Ya Disk and removed saving to a folder on the PC

--- main.py
+++ main.py
@@ -15,12 +15,16 @@ class VkPhotoInYaDisk:
     url = 'https://api.vk.com/method/'
 
     def __init__(self, ya_token, id_vk):
-        self.params = {
+        self.token_ya = ya_token
+        self.id_vk = id_vk
+        self.params_vk = {
             'access_token': parameters_dict['token VK'],
             'v': '5.131'
         }
-        self.token_ya = ya_token
-        self.id_vk = id_vk
+        self.headers_ya = {
+            'Content-Type': 'application/json',
+            'Authorization': 'OAuth {}'.format(self.token_ya)
+        }
 
     def _get_url_photo_vk(self):
         photo_download_url = self.url + 'photos.get'
@@ -30,15 +34,17 @@ class VkPhotoInYaDisk:
             'extended': 1,
             'count': 50
         }
-        req = requests.get(photo_download_url, params={**self.params, **photo_download_parameters})
+        req = requests.get(photo_download_url, params={**self.params_vk, **photo_download_parameters})
         return req.json()
 
     def download_photo_and_create_json(self):
         items = self._get_url_photo_vk()['response']['items']
         name_dir = str(date.today())
-        if not os.path.isdir(name_dir):
-            os.mkdir(name_dir)
-        os.chdir(name_dir)
+        create_dir_url = 'https://cloud-api.yandex.net/v1/disk/resources'
+        params_create_dir = {
+            'path': str(name_dir)
+        }
+        requests.put(create_dir_url, headers=self.headers_ya, params=params_create_dir)
         write_json = []
         info_name = {}
         for like in items:
@@ -46,26 +52,28 @@ class VkPhotoInYaDisk:
                 info_name[like['likes']['count']] = 1
             else:
                 info_name[like['likes']['count']] += 1
-
         for item in items:
             name = item['likes']['count']
             date_add = datetime.fromtimestamp(item['date']).strftime('%d-%m-%y %H-%M-%S')
             if info_name[name] > 1:
                 name = str(name) + ' ' + str(date_add)
             url_photo_max_size = item['sizes'][-1]['url']
-            with open(str(name) + '.jpg', 'wb') as file_photo:
-                content = requests.get(url_photo_max_size)
-                file_photo.write(content.content)
+            url_upload_to_ya = 'https://cloud-api.yandex.net/v1/disk/resources/upload'
+            params_upload_to_ya = {
+                'path': f'{name_dir}/{str(name)}',
+                'url': url_photo_max_size
+            }
+            requests.post(url_upload_to_ya, headers=self.headers_ya, params=params_upload_to_ya)
             height = item['sizes'][-1]['height']
             width = item['sizes'][-1]['width']
             add_write_json = {
-                'file_name': str(name) + '.jpg',
-                'size': str(height) + 'x' + str(width),
+                'file_name': f'{str(name)}.jpg',
+                'size': f'{str(height)}x{str(width)}',
             }
             write_json.append(add_write_json)
         with open('foto_info.json', 'w') as file_json:
             json.dump(write_json, file_json, indent=4)
 
 
-Durov = VkPhotoInYaDisk('asga', 552934290)
+Durov = VkPhotoInYaDisk(parameters_dict['token Ya'], 552934290)
 Durov.download_photo_and_create_json()
